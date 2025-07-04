9e4d1eb89ca69fbe9555db68fa4f77f4115d2c73
erixian <cibercianocode@gmail.com>
2025-07-04 11:44:27 -0300

    conta banco finalizada

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
-public class App {
+import java.util.Scanner;
+
+public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        var sc = new Scanner(System.in);
+
+        String nomeCliente;
+        int numeroConta; 
+        String agencia;
+        float saldo;
+
+        System.out.println("Por favor, digite o nome do cliente:");
+        nomeCliente = sc.nextLine();
+        System.out.println("Por favor, digite o número da conta:");
+        numeroConta = sc.nextInt();
+        System.out.println("Por favor, digite o número da agência:");
+        agencia = sc.next();
+        sc.nextLine(); 
+        System.out.println("Por favor, digite o saldo da conta:");
+        saldo = sc.nextFloat();
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo R$mar%.2f já está disponível para saque.\n", 
+                          nomeCliente, agencia, numeroConta, saldo);
+
     }
 }
